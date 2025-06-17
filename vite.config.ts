232c7430b06184075afe3b0b4657c2e96ef02869
Mikael Pettersson <evanion@icloud.com>
2025-06-17 23:41:58 +0200

    integrate sentry

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,25 @@
 import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
+import {
+  sentryReactRouter,
+  type SentryReactRouterBuildOptions,
+} from "@sentry/react-router";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
-export default defineConfig({
-  plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
-});
+const sentryConfig: SentryReactRouterBuildOptions = {
+  org: "outcasts-c1",
+  project: "website",
+  // An auth token is required for uploading source maps.
+  authToken: process.env.SENTRY_AUTH_TOKEN || "",
+  // ...
+};
+
+export default defineConfig((config) => ({
+  plugins: [
+    tailwindcss(),
+    reactRouter(),
+    sentryReactRouter(sentryConfig, config),
+    tsconfigPaths(),
+  ],
+}));
